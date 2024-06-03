cb253ab47d089fe39b982d004a848cee813e3490
Shisanucha Chengsamo <shitsanucha@gmail.com>
2024-06-03 16:27:27 +0700

    crate connected succefully and crated entities

--- src/main/java/int103/Repository/BaseRepository.java
+++ src/main/java/int103/Repository/BaseRepository.java
@@ -1,10 +1,5 @@
 package int103.Repository;
 
-public class BaseRepository<T, > {
-    private String entityName;
-
-    public BaseRepository(String entityName){
-        this.entityName = entityName;
-    }
+public class BaseRepository {
 
 }
