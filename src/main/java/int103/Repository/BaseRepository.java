bba5aa8eb1c4753d1bab7c6ab8ef79ba643909a4
Shisanucha Chengsamo <shitsanucha@gmail.com>
2024-06-03 14:59:03 +0700

    create entities

--- src/main/java/int103/Repository/BaseRepository.java
+++ src/main/java/int103/Repository/BaseRepository.java
@@ -1,5 +1,10 @@
 package int103.Repository;
 
-public class BaseRepository {
+public class BaseRepository<T, > {
+    private String entityName;
+
+    public BaseRepository(String entityName){
+        this.entityName = entityName;
+    }
 
 }
