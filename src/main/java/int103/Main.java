ea491fc5879a76e8d5ec47a112a7cbe9bbf63d1a
Shisanucha Chengsamo <shitsanucha@gmail.com>
2024-06-05 17:35:04 +0700

    Created method Addcourse, deleteCourse, listCourse, listStudent, Register, stuentInfo

--- src/main/java/int103/Main.java
+++ src/main/java/int103/Main.java
@@ -1,7 +1,11 @@
 package int103;
 
+import int103.service.*;
+
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("อะไรครับเนี่ย!!!");
+
     }
 }
