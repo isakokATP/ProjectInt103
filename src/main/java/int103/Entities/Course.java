52bd80919cf9308bdfeb8e09aa79113715baa05a
Ratikorn Damgurnglubpanavanit <ratikorn.damg@kmutt.ac.th>
2024-06-09 02:34:52 +0700

    merge from main (fix database)

--- src/main/java/int103/Entities/Course.java
+++ src/main/java/int103/Entities/Course.java
@@ -1,6 +1,5 @@
 package int103.entities;
 
-<<<<<<< HEAD
 import java.io.Serializable;
 
 public class Course implements Serializable {
@@ -18,43 +17,13 @@ public class Course implements Serializable {
 
     public String getName() {
         return name;
-=======
-public class Course {
-    private String courseId;
-    private String courseName;
-
-    public Course(String courseId, String courseName) {
-        this.courseId = courseId;
-        this.courseName = courseName;
-    }
-
-    public String getCourseId() {
-        return courseId;
-    }
-
-    public void setCourseId(String courseId) {
-        this.courseId = courseId;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
->>>>>>> Chaiy0
     }
 
     @Override
     public String toString() {
-<<<<<<< HEAD
         return "Course{" +
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
                 '}';
-=======
-        return "Course [courseId=" + courseId + ", courseName=" + courseName + "]";
->>>>>>> Chaiy0
     }
 }
-
--- src/main/java/int103/Entities/Register.java
+++ src/main/java/int103/Entities/Register.java
@@ -2,17 +2,10 @@ package int103.entities;
 
 public class Register {
     private int registrationId;
-<<<<<<< HEAD
     private Long studentId;
     private String courseId;
 
     public Register(int registrationId, Long studentId, String courseId) {
-=======
-    private String studentId;
-    private String courseId;
-
-    public Register(int registrationId, String studentId, String courseId) {
->>>>>>> Chaiy0
         this.registrationId = registrationId;
         this.studentId = studentId;
         this.courseId = courseId;
@@ -22,23 +15,11 @@ public class Register {
         return registrationId;
     }
 
-    public void setRegistrationId(int registrationId) {
-        this.registrationId = registrationId;
-    }
-
-<<<<<<< HEAD
     public Long getStudentId() {
         return studentId;
     }
 
     public void setStudentId(Long studentId) {
-=======
-    public String getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(String studentId) {
->>>>>>> Chaiy0
         this.studentId = studentId;
     }
 
@@ -52,15 +33,10 @@ public class Register {
 
     @Override
     public String toString() {
-<<<<<<< HEAD
         return "Register{" +
                 "registration_id=" + registrationId +
                 ", student_id=" + studentId +
                 ", course_id='" + courseId + '\'' +
                 '}';
-=======
-        return "Registration [registrationId=" + registrationId + ", studentId=" + studentId + ", courseId=" + courseId + "]";
->>>>>>> Chaiy0
     }
 }
-
